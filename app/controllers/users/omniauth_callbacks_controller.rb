5d3fc4e7a6cb2f71084c58c5bda9b3b97464bb6b
David Uhlig <david.uhlig@gmail.com>
2024-10-06 00:14:14 +0200

    Refactor `Users::OmniauthCallbacksController`

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,13 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def rocketchat
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    auth = request.env["omniauth.auth"]
+    @user = User.from_omniauth(auth)
 
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Rocket.Chat"
       sign_in_and_redirect @user, event: :authentication
     else
-      session["devise.rocketchat_data"] = request.env["omniauth.auth"].except(:extra)
+      session["devise.rocketchat_data"] = auth.except(:extra)
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
