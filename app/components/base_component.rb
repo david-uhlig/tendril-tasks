a3774ef025f56243e6a352e09e783b713f1fdcbc
David Uhlig <david.uhlig@gmail.com>
2024-10-07 14:59:11 +0200

    Add `BaseComponent`
    Creates the root component for all other components in the application.

---
+++ app/components/base_component.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Base component from which all other components inherit
+#
+# Conventions:
+# * Component names end in -Component
+# * Component module names are plural, as for controllers and jobs, e.g.
+#   `Users::AvatarComponent`
+# * Name components for what they render, not what they accept, e.g.
+#   `AvatarComponent` instead of `UserComponent`
+#
+# @see https://viewcomponent.org/guide/getting-started.html#conventions
+class BaseComponent < ViewComponent::Base
+end
