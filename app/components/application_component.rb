d8a7fd1ae553cfb7bb66c7276120db46b0239ec0
David Uhlig <david.uhlig@gmail.com>
2024-10-08 13:57:50 +0200

    Rename `BaseComponent` to `ApplicationComponent`
    ApplicationComponent is the default root component for ViewComponent. It can be configured, but it's easier to use the default.

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Base component from which all other components inherit
+# Root component from which all other components inherit
 #
 # Conventions:
 # * Component names end in -Component
@@ -10,7 +10,7 @@
 #   `AvatarComponent` instead of `UserComponent`
 #
 # @see https://viewcomponent.org/guide/getting-started.html#conventions
-class BaseComponent < ViewComponent::Base
+class ApplicationComponent < ViewComponent::Base
   include Primer::FetchOrFallbackHelper
   include Primer::ClassNameHelper
 end
