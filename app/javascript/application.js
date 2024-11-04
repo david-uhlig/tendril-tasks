4e8f679089c028566c539e89dd6a18a151fddfe5
David Uhlig <david.uhlig@gmail.com>
2024-11-04 20:42:48 +0100

    Reattach Flowbite after same page events
    Flowbite JS does not initialize after events like 422 Unprocessable Entity. With Turbo these events do not trigger a full page reload. When "leaving" the page through a POST request Flowbite JS events would get detached but fail to get reattached when Turbo processes the partial rebuild of the page.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,12 @@ import "@hotwired/turbo-rails"
 import "flowbite"
 import "controllers"
 
+/**
+ * Reattach Flowbite Turbo after events like 422 Unprocessable Entity
+ *
+ * see: https://github.com/themesberg/flowbite/issues/88#issuecomment-1962238351
+ */
+window.document.addEventListener("turbo:render", (_event) => {
+    window.initFlowbite();
+});
+
