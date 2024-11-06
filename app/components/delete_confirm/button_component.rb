81c69a8fe92ed1003979d67c769f9fec76744b5f
David Uhlig <david.uhlig@gmail.com>
2024-11-06 19:14:00 +0100

    Add `DeleteConfirm` components
    `DeleteConfirm::ButtonComponent` displays a button that opens the hidden modal when clicked.
    
    `DeleteConfirm::ModalComponent` renders the modal with customizable heading, body text, an abort and submit button.

---
+++ app/components/delete_confirm/button_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class DeleteConfirm::ButtonComponent < ApplicationComponent
+  attr_reader :target_modal_id
+
+  def initialize(text:, target_modal_id: nil)
+    @text = text
+    @target_modal_id = target_modal_id.presence || "delete-confirm-#{SecureRandom.alphanumeric(5)}"
+  end
+
+  def call
+    render ButtonComponent
+      .new({ class: "w-full",
+             "data-modal-target": @target_modal_id,
+             "data-modal-toggle": @target_modal_id,
+             type: "button"
+           }, scheme: :red, size: :large)
+      .with_content(@text)
+  end
+end
---
+++ app/components/delete_confirm/modal_component.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class DeleteConfirm::ModalComponent < ApplicationComponent
+  attr_reader :id
+
+  renders_one :heading_slot, ->(text = "Wirklich löschen?") do
+    tag.h3 class: "text-xl font-semibold text-gray-900 dark:text-white" do
+      text
+    end
+  end
+  alias heading with_heading_slot
+
+  renders_many :texts, ->(text = nil) do
+    tag.p class: "text-base leading-relaxed text-gray-500 dark:text-gray-400" do
+      text
+    end
+  end
+  alias text with_text
+
+  renders_one :abort_button_slot, ->(text = "Abbrechen") do
+    ButtonComponent
+      .new({ class: "w-full lg:w-auto", "data-modal-hide": @id, type: "reset" }, scheme: :light)
+      .with_content(text)
+  end
+  alias abort_button with_abort_button_slot
+
+  renders_one :confirm_button_slot, ->(text = "Endgültig löschen") do
+    ButtonComponent
+      .new({ class: "w-full lg:w-auto", "data-modal-hide": @id }, scheme: :red)
+      .with_content(text)
+  end
+  alias confirm_button with_confirm_button_slot
+
+  def initialize(id: nil, delete_path:)
+    @id = id.presence || "delete-confirm-#{SecureRandom.alphanumeric(5)}"
+    @delete_path = delete_path
+  end
+end
