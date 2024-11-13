2fb2d760f192dd8db3534a12f0233d4206f86cfb
David Uhlig <david.uhlig@gmail.com>
2024-11-13 12:40:50 +0100

    Refactor `FieldError` component
    Move to the `Form` namespace.

---
+++ app/components/form/field_error_component.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Form
+  class FieldErrorComponent < ApplicationComponent
+    def initialize(obj_with_errors = nil, error_field = nil)
+      @errors = obj_with_errors&.errors
+      @error_field = error_field
+    end
+
+    def has_errors?
+      @errors.any? && @error_field.present? && @errors[@error_field].present?
+    end
+  end
+end
