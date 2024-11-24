deaf50c1f27bb666fadff1530b427ed7c4639729
David Uhlig <david.uhlig@gmail.com>
2024-11-24 12:05:10 +0100

    Refactor `Form::BaseField` component
    Allow to hide the label. Useful for contexts like filter bars where the label should be omitted.
    
    Improve documentation.

--- app/components/form/base_field_component.rb
+++ app/components/form/base_field_component.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 module Form
+  # Base structure for form field components.
+  #
+  # It provides a consistent way to render fields with their associated label,
+  # error messages, and custom options.
+  #
+  # Example Usage:
+  #   <%= render Form::BaseFieldComponent.new(form, :email, "Email Address", class: "custom-class") do %>
+  #     <%= form.text_field :email %>
+  #   <% end %>
   class BaseFieldComponent < ApplicationComponent
+    # @param form [Object] Form object (typically a `form_with` or `form_for` object) used for generating fields.
+    # @param attribute [Symbol] The attribute from the model associated with the form field (e.g., :email).
+    # @param label [String, nil, false] The label text for the field. Pass `nil` to use the default label, or `false` to omit the label entirely.
+    # @param error_field_attribute [Symbol] (Optional) The attribute used for displaying error messages. Defaults to the `attribute` parameter if not provided.
+    # @param options [Hash] (Optional) A hash of additional HTML options for the container div.
+    #
+    # The block passed to this component will be rendered as the actual field content (e.g., text field, checkbox, etc.).
     def initialize(form, attribute, label = nil, error_field_attribute = nil, **options)
       @form = form
       @attribute = attribute
