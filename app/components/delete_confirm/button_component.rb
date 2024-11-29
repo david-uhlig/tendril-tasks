595eacf41d518d2f1e3a1cb720da17dc4f6fd6b3
David Uhlig <david.uhlig@gmail.com>
2024-11-29 16:14:43 +0100

    Refactor `Button` component to take options parameters

--- app/components/delete_confirm/button_component.rb
+++ app/components/delete_confirm/button_component.rb
@@ -4,21 +4,27 @@ module DeleteConfirm
   class ButtonComponent < ApplicationComponent
     attr_reader :target_modal_id
 
-    def initialize(text:, target_modal_id: nil)
+    def initialize(text:, target_modal_id: nil, **options)
       @text = text
       @target_modal_id = target_modal_id.presence || "delete-confirm-#{SecureRandom.alphanumeric(5)}"
+      @options = build_options(options)
     end
 
     def call
-      render ::ButtonComponent.new(
-               scheme: :red,
-               size: :large,
-               class: "w-full",
-               data: { "modal-target": @target_modal_id,
-                       "modal-toggle": @target_modal_id
-               },
-               type: :button
-             ).with_content(@text)
+      render ::ButtonComponent.new(**@options).with_content(@text)
+    end
+
+    private
+
+    def build_options(options)
+      options.stringify_keys!
+      options["scheme"] ||= :red
+      options["size"] ||= :large
+      options["class"] ||= "w-full"
+      options["type"] ||= :button
+      options["data"] = { "modal-target": @target_modal_id, "modal-toggle": @target_modal_id }.merge(options.delete("data") || {})
+      options.symbolize_keys!
+      options
     end
   end
 end
