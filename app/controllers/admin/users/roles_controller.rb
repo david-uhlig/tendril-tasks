95722519e1def371f008d8ac371d8eb9d9e7bb26
David Uhlig <david.uhlig@gmail.com>
2024-12-06 11:32:07 +0100

    Prevent permission changes on current_user

--- app/controllers/admin/users/roles_controller.rb
+++ app/controllers/admin/users/roles_controller.rb
@@ -5,11 +5,18 @@ module Admin
       # TODO add authorization
       def update
         authorize! :update, :user_roles
+
         # Refuse to remove the last admin
         if params[:role] != "admin" &&
           @user.admin? &&
           User.where(role: :admin).count <= 1
           render :last_admin_error
+          return
+        end
+
+        # Refuse to change permissions on current_user
+        if @user == current_user
+          render :cannot_change_current_user
           return
         end
 
