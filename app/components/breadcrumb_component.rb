1dd2cfe5aa619744134d2d25e976a549fa7be925
David Uhlig <david.uhlig@gmail.com>
2024-12-14 13:16:04 +0100

    Refactor `Breadcrumb` component
    Adds a `home` slot to display the link to the page root in a consistent way across all breadcrumb navigations.

--- app/components/breadcrumb_component.rb
+++ app/components/breadcrumb_component.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class BreadcrumbComponent < ApplicationComponent
+  renders_one :home_slot, ->(title = "Start", href: root_path, icon: :home) {
+    Breadcrumb::LinkComponent.new(title, href: href, icon: icon)
+  }
+  alias home with_home_slot
+
   renders_many :sections, Breadcrumb::LinkComponent
   alias section with_section
 end
