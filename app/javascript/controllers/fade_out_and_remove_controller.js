dec8ffffefe3d28f3e5951727672ab5a062b5ee7
David Uhlig <david.uhlig@gmail.com>
2024-12-15 18:04:23 +0100

    Increase notification fadeout delay

--- app/javascript/controllers/fade_out_and_remove_controller.js
+++ app/javascript/controllers/fade_out_and_remove_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   connect() {
     setTimeout(() => {
       this.element.classList.add("opacity-0");
-    }, 1000);
+    }, 3000);
 
     setTimeout(() => {
       this.element.remove();
