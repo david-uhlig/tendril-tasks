2512e3b6d08790cbad849667b18a336462d931ac
David Uhlig <david.uhlig@gmail.com>
2024-12-23 10:38:22 +0100

    Monkey patch Anyway Config to make it work with Kamal

--- config/configs/base_config.rb
+++ config/configs/base_config.rb
@@ -2,6 +2,15 @@
 
 # Base class for application config classes
 class BaseConfig < Anyway::Config
+  def validate_required_attributes!
+    # Disable required fields during asset precompilation
+    # TODO remove this monkey patch when anyway_config will support SECRET_KEY_BASE_DUMMY
+    # see: https://github.com/palkan/anyway_config/pull/152
+    return if ENV["SECRET_KEY_BASE_DUMMY"] == "1"
+
+    super
+  end
+
   class << self
     # Make it possible to access a singleton config instance
     # via class methods (i.e., without explicitly calling `instance`)
