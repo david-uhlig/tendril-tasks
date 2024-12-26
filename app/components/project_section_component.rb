51fd7207150290739c593ec9b8f6dd76e364b33a
David Uhlig <david.uhlig@gmail.com>
2024-12-26 22:19:16 +0100

    Refactor `ProjectSection` component into TendrilTasks::Projects namespace

--- app/components/project_section_component.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ProjectSectionComponent < ApplicationComponent
-  def initialize(project)
-    @project = project
-  end
-end
---
+++ app/components/tendril_tasks/projects/section.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module TendrilTasks
+  module Projects
+    class Section < ApplicationComponent
+      with_collection_parameter :collection_item
+
+      def initialize(project = nil, **options)
+        @project = options.delete(:collection_item) ||
+          project ||
+          (raise ArgumentError, "project is required")
+      end
+    end
+  end
+end
