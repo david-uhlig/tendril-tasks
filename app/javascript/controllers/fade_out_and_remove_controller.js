6052f67ba03d41e347504e8cab4de734c58b3b2e
David Uhlig <david.uhlig@gmail.com>
2025-01-05 11:54:26 +0100

    Make notification stackable

--- app/javascript/controllers/fade_out_and_remove_controller.js
+++ app/javascript/controllers/fade_out_and_remove_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     setTimeout(() => {
-      this.element.classList.add("opacity-0");
+      this.element.classList.add("animate-pulse");
     }, 3000);
 
     setTimeout(() => {
