2b1227819c8963229196181b24c5131541dd7605
David Uhlig <david.uhlig@gmail.com>
2025-01-08 15:05:18 +0100

    Fix RocketChatConfig.host not present in test environment.

--- config/configs/rocket_chat_config.rb
+++ config/configs/rocket_chat_config.rb
@@ -15,7 +15,7 @@ class RocketChatConfig < BaseConfig
   def http_host
     if Rails.env == "test"
       "http://example.com"
-    elsif host.starts_with("http://", "https://")
+    elsif host.starts_with?("http://", "https://")
       host
     else
       "https://#{host}"
