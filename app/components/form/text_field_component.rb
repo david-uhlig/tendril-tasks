06e1a154b23e55990c13fc431141f8c1610ec4c6
David Uhlig <david.uhlig@gmail.com>
2025-01-18 20:57:54 +0100

    Make `TextField` component configurable

--- app/components/form/text_field_component.rb
+++ app/components/form/text_field_component.rb
@@ -2,15 +2,22 @@
 
 module Form
   class TextFieldComponent < TendrilTasks::Component
-    def initialize(form, attribute, label = nil)
+    style :base,
+          "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    def initialize(form, attribute, label = nil, **options)
       @form = form
       @attribute = attribute
       @label = label
+
+      options.symbolize_keys!
+      options[:class] = styles(base: true,
+                               custom: options.delete(:class))
+      @options = options
     end
 
     def call
       render BaseFieldComponent.new(@form, @attribute, @label) do
-        @form.text_field @attribute, class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        @form.text_field @attribute, **@options
       end
     end
   end
