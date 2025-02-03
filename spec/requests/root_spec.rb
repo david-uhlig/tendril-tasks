0d0e9b08fac060eb2b733a783d46aa542dba675a
David Uhlig <david.uhlig@gmail.com>
2025-02-03 23:11:07 +0100

    Refactor root request specs

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -2,11 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Root path", type: :request do
   describe "GET /" do
-    it 'loads successfully' do
-      expect {
-        get root_path
-      }.not_to raise_error
-      expect(response).to have_http_status(:success)
+    context "as a visitor" do
+      it "loads the root page" do
+        expect {
+          get root_path
+        }.not_to raise_error
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "as a user" do
+      let(:user) { create(:user) }
+
+      it "loads the root page" do
+        login_as(user)
+
+        expect {
+          get root_path
+        }.not_to raise_error
+
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
