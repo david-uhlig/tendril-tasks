2b87c5ad3f98a81e822af6267ba23209ca66f729
David Uhlig <david.uhlig@gmail.com>
2025-02-14 09:33:21 +0100

    Rich text styling adjustments

--- app/components/gustwave/rich_text.rb
+++ app/components/gustwave/rich_text.rb
@@ -4,14 +4,15 @@ module Gustwave
   # Use to render rich text content
   class RichText < Gustwave::Component
     style :base,
-          "trix-content"
+          "*:hyphens-auto"
 
     def initialize(text = nil, **options)
-      @text = text
+      @text = text.to_s.html_safe
 
       options.symbolize_keys!
       options[:class] = styles(base: true,
                                custom: options.delete(:class))
+      options[:lang] ||= I18n.default_locale
       @options = options
     end
 
