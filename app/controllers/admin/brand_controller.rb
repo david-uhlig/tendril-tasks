29fdee8d960b95069ed1678253aabb75fab64c27
David Uhlig <david.uhlig@gmail.com>
2026-09-05 21:09:12 +0200

    Refactor BrandController to inherit from AdminController and add request specs
    Signed-off-by: David Uhlig <david.uhlig@gmail.com>

--- app/controllers/admin/brand_controller.rb
+++ app/controllers/admin/brand_controller.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 module Admin
-  class BrandController < ApplicationController
-    # TODO add request specs
-
+  class BrandController < AdminController
     def edit
-      authorize! :update, :admin_settings
-
       @brand = ::Brand.new
     end
   end
